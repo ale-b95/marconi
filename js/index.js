48f17f934e8c9a9a5d0f55c403ef4344899b2507
Alessandro Ballerini <alessandroballerini95@gmail.com>
2018-02-17 18:06:25 +0100

    created prof data visualization script

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 $(function() {
-  var uid = 0;
 
-  var prof_name = $("#add_prof_name")[0];
-  var prof_surname = $("#add_prof_surname")[0];
-  var prof_pwd = $("#add_prof_pwd")[0];
-  var prof_pwd_r = $("#add_prof_pwd_r")[0];
-  var prof_admin = $("#add_prof_admin");
 
+//inserisci docente
   $("#add_prof_button").click(function() {
-    if (prof_pwd.value == prof_pwd_r.value) {
-      writeProfData(uid, prof_name.value, prof_surname.value, prof_admin.is(":checked"), prof_pwd.value);
-      uid++;
+    if ($("#add_prof_pwd")[0].value ==  $("#add_prof_pwd_r")[0].value) {
+      writeProfData(
+        $("#add_prof_name")[0].value,
+        $("#add_prof_surname")[0].value,
+        $("#add_prof_admin").is(":checked"),
+        $("#add_prof_pwd")[0].value);
       $('#add_prof_form')[0].reset();
     } else {
       alert("le password non corrispondono");
@@ -18,13 +16,36 @@ $(function() {
     }
   });
 
-  function writeProfData(profId, name, surname, admin, pwd) {
-    var dbRef = firebase.database().ref().child("docenti").push().set({
+  function writeProfData( name, surname, admin, pwd) {
+    var dbRef = firebase.database().ref().child('docenti').push().set({
       nome: name,
       cognome: surname,
       admin: admin,
       password: pwd
     });
-    alert ("Docente inserito");
+    //alert ("Docente inserito");
   }
+
+//visualizza docenti
+  showProfList ();
+  function showProfList () {
+    const dbRef_doc = firebase.database().ref().child('docenti').orderByKey();
+    dbRef_doc.on('child_added', snap => {
+      $("#prof_list").append("<ul id='"+snap.key+"'>Id: "+snap.key+"</ul><br/>");
+      snap.forEach(childSnap => {
+        var key = childSnap.key;
+        var childData = childSnap.val();
+        if (key == "nome") {
+          $("#"+snap.key+"").append("<li>Nome: "+childData+"</li>");
+        } else if (key == "cognome") {
+          $("#"+snap.key+"").append("<li>Cognome: "+childData+"</li>");
+        } else if (key == "password") {
+          $("#"+snap.key+"").append("<li>Password: "+childData+"</li>");
+        } else if (key == "admin") {
+          $("#"+snap.key+"").append("<li>Admin: "+childData+"</li>");
+        }
+      });
+    });
+  }
+
 });
