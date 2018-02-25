63d240c4ff0327b0146581baedb663b6974a68c1
Alessandro Ballerini <alessandroballerini95@gmail.com>
2018-02-25 17:30:07 +0100

    working on institute login

--- js/homepage.js
+++ js/homepage.js
@@ -1,5 +1,4 @@
 $(function () {
-
   $("#new_institute_button").on('click', () => {
     $("#user_page").hide();
     $("#new_institute").show();
@@ -8,6 +7,7 @@ $(function () {
   $("#fromNItoUP").on('click', () => {
     $("#new_institute").hide();
     $("#user_page").show();
+    getInstituteList();
   })
 
   $("#create_institute_button").on('click', () => {
@@ -15,14 +15,18 @@ $(function () {
   })
 
   $("#log_institute_button").on('click', () => {
+    getInstituteList();
     $("#user_page").hide();
     $("#log_institute").show();
-    getInstituteList();
   })
 
   $("#fromLItoUP").on('click', () => {
     $("#log_institute").hide();
     $("#user_page").show();
+  })
+
+  $("#choose_inst").on('click', () => {
+    logOnInstitute();
   })
 
   function createInstitute() {
@@ -37,7 +41,9 @@ $(function () {
     var inst_id = inst_ref.key;
 
     ref.child('institute/' + inst_id + '/user/'+ auth.currentUser.uid).set({
-      name: auth.currentUser.displayName
+      name: auth.currentUser.displayName,
+      admin: true,
+      confirmed: true
     });
 
     ref.child('user/'+ auth.currentUser.uid +'/institute').update({
@@ -48,11 +54,50 @@ $(function () {
     $("#user_page").show();
   }
 
-  function getInstituteList() {
+  function getInstituteList(callback) {
+    $("#my_institutes").empty();
+    $("#all_institutes").empty();
+
+    const user = firebase.auth().currentUser;
     const dbRef = firebase.database().ref();
+
+    var user_inst = dbRef.child('user/'+ user.uid + '/institute/').orderByKey();
+    user_inst.once('value').then(snap => {
+      snap.forEach(childSnap => {
+        var name = childSnap.val();
+        var key = childSnap.key;
+        $("#my_institutes").append('<option value="'+ key +'">'+ name +'</option>');
+      });
+    });
+
+    var global_inst = dbRef.child('institute/').orderByKey();
+    global_inst.once('value').then(snap => {
+      snap.forEach(childSnap => {
+        var name = childSnap.child('name').val();
+        var key = childSnap.key;
+        $("#all_institutes").append('<option value="'+ key +'">'+ name +'</option>');
+      });
+    });
   }
 
+  function logOnInstitute() {
+    var auth_user;
+    var inst_id = $("#select_institute").val();
+    var inst_name = $("#select_institute").find(':selected').text();
 
+    const user = firebase.auth().currentUser;
+    const dbRef = firebase.database().ref();
 
+    // add institute to user
+    dbRef.child('user/' + user.uid + '/institute/').update({
+      [inst_id] : inst_name
+    });
 
+    // add user to institute
+    dbRef.child('institute/' + inst_id + '/user/' + user.uid).update({
+      name: user.displayName
+    });
+
+    // check if authorized user
+  }
 });
