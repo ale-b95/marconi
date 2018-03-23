f2e34dafc5a434cb30a7f3b1ec567a5b6eabb7f8
Alessandro Ballerini <alessandroballerini95@gmail.com>
2018-03-23 17:58:00 +0100

    commit text is useless

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,15 +1,4 @@
 $(function () {
-
-  /*
-  TODO
-    quando si selezionano delle ore SENZA PRENOTARE, si torna indietro, si selezionano altre ore e si prenota
-    si prenotano anche le ore non prenotate (ma solo lelezionate) prima di uscire
-
-    una volta fatta una prenotazione non vengono mostrati i nuovi colori immediatamente ma solo se si esce
-    e rientra
-  */
-
-
   var date = null;
 
   var day;
@@ -23,7 +12,8 @@ $(function () {
   var mb_selected_rows = 0;
 
   var selected_hours = [];
-  var arr = {};
+  var prenotation_id = [];
+
 
   $('#classroom_datepicker').datepicker({
     format: 'dd/mm/yyyy',
@@ -52,11 +42,13 @@ $(function () {
   function selectClassroom() {
     classroom_id = $("#select_classroom").val();
     classroom_name = $("#select_classroom").find(':selected').text();
+
   }
 
   function loadClassroomSchedule() {
     selectDate();
     selectClassroom();
+    var dbHoursRef = {};
 
     if (classroom_id != 'Select a Classroom' && date) {
       $("#schedule_table_body").empty();
@@ -69,10 +61,10 @@ $(function () {
         +classroom_id+'/'
         );
 
-      //aggiorna array con key delle prenotaizoni
+      //aggiorna dbHoursRefay con key delle prenotaizoni
       pRef.once('value', snap => {
         snap.forEach(childSnap => {
-          arr['h_'+childSnap.key] = childSnap.val();
+          dbHoursRef['h_'+childSnap.key] = childSnap.val();
         });
       }).then(() => {
         for (var hour = 8; hour<16; hour++) {
@@ -80,14 +72,14 @@ $(function () {
               '<tr class="clickable-row" id="hid_'+hour+'" value="'+hour+'">'+
               '<th>'+hour+':00</th><td></td>'+
             '</tr>');
-            loadBookedHours(hour, arr);
+            loadBookedHours(hour, dbHoursRef);
         }
       });
     }
   }
 
-  function loadBookedHours(hour, arr) {
-    if (arr['h_'+hour]) {
+  function loadBookedHours(hour, dbHoursRef) {
+    if (dbHoursRef['h_'+hour]) {
       var teacher_name;
       var class_name;
       var event_name;
@@ -98,7 +90,7 @@ $(function () {
       firebase.database().ref('institute/'
         +INSTITUTE_ID
         +'/prenotation_list/'
-        + arr['h_' + hour]
+        + dbHoursRef['h_' + hour]
       ).once('value', father => {
         father.forEach(child => {
           if (child.key == 'teacher') {
@@ -147,14 +139,16 @@ $(function () {
     } else if ($(this).hasClass('mybook') && cs_selected_rows == 0) {
       $(this).addClass('mybook_selected');
       $(this).removeClass('mybook');
+      selected_hours.push($(this).attr('value'));
       increase_mb_select(+1);
     } else if ($(this).hasClass('mybook_selected')) {
       $(this).removeClass('mybook_selected');
       $(this).addClass('mybook');
+      idx = selected_hours.indexOf($(this).attr('value'));
+      if (idx >= 0) selected_hours.splice(idx, 1);
       increase_mb_select(-1);
     }
   });
-
 
   function increase_mb_select(x) {
     mb_selected_rows += x;
@@ -164,14 +158,6 @@ $(function () {
       $('#book_prenotation_btn').text('Book');
     }
   }
-
-  $('.cs_back_btn').on('click', () => {
-    cs_selected_rows = 0;
-    mb_selected_rows = 0;
-    $('#book_prenotation_btn').text('Book');
-    arr = {};
-    console.log(arr);
-  });
 
   $('#book_prenotation_btn').on('click', () => {
     user = firebase.auth().currentUser;
@@ -201,10 +187,99 @@ $(function () {
             [selected_hours[i]] : prenotation_inlist.key
           });
         }
+        selected_hours = [];
+        prenotation_id = [];
+        loadClassroomSchedule();
+        cs_selected_rows = 0;
+        mb_selected_rows = 0;
+        $('#book_prenotation_btn').text('Book');
     } else if (class_name == 'Select a Class' && cs_selected_rows > 0){
       alert ('Select a class');
     } else if (mb_selected_rows > 0) {
-      alert('TO DO');
+      // dichiaro array per contenere ID PRENOTAZIONI RIMOSSE
+
+
+      for (var i = 0; i < selected_hours.length; i++) {
+        var hour_prenotation_ref = firebase.database().ref('institute/'+INSTITUTE_ID+
+        '/prenotation/'+year+'/'+month+'/'+day+'/'+classroom_id+'/');
+
+        hour_prenotation_ref.once('value', snap => {
+          snap.forEach(childSnap => {
+            console.log(selected_hours[i]);
+            if (childSnap.key == selected_hours[i]);
+            prenotation_id.push(childSnap.val());
+          });
+        }).then(hour_prenotation_ref.child(selected_hours[i]).remove());
+      }
+
+      console.log(prenotation_id);
+
+
+      /*
+      // dichiaro array per contenere ID PRENOTAZIONI RIMOSSE
+      var prenotation_id = [];
+
+      // referenza alle prenotazioni in data 'date' e aula scelta
+      var prenotation_ref = firebase.database().ref('institute/'+INSTITUTE_ID+
+      '/prenotation/'+year+'/'+month+'/'+day+'/'+classroom_id+'/');
+
+      // ciclo per le ore selezionate
+      for (var i = 0; i < selected_hours.length; i++) {
+
+        //referenza alla prima ora selezionata
+        var hour_ref = prenotation_ref.child(selected_hours[i]);
+        hour_ref.once('value', snap => {
+
+          //inserisco ID PRENOTAZIONE DA RIMUOVERE
+          prenotation_id[prenotation_id.length] = snap.val();
+        }).then(() => {
+
+          //rimuovo prenotazione
+          hour_ref.remove();
+          console.log('rimuovo prenotazione');
+        });
+      }
+
+      // ottengo referenza a prenotation list
+      var p_list_ref = firebase.database().ref('institute/'+INSTITUTE_ID
+      +'/prenotation_list/');
+
+      //ciclo gli id delle prenotazioni eliminate
+      for (var i = 0; i < prenotation_id.length; i++) {
+        var element_occurence = false;
+
+        //per ogni elemento dell'array se non esiste un child con lo stesso id elimino la prenotazione
+        prenotation_ref.once('value', snap => {
+          snap.forEach(childSnap => {
+            if (childSnap.val() == prenotation_id[i]);
+            element_occurence = true;
+          });
+        }).then(() => {
+          if (element_occurence == false) {
+            p_list_ref.child(prenotation_id[i]).remove();
+          }
+        });
+      }
+
+      loadClassroomSchedule();
+      selected_hours = [];
+      cs_selected_rows = 0;
+      mb_selected_rows = 0;
+      $('#book_prenotation_btn').text('Book');
+      */
+      loadClassroomSchedule();
+      selected_hours = [];
+      prenotation_id = [];
+      cs_selected_rows = 0;
+      mb_selected_rows = 0;
+      $('#book_prenotation_btn').text('Book');
     }
+  });
+
+  $('.cs_back_btn').on('click', () => {
+    cs_selected_rows = 0;
+    mb_selected_rows = 0;
+    $('#book_prenotation_btn').text('Book');
+    selected_hours = [];
   });
 });
